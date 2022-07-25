22acc0f876aa450df14ad520bbb9e3ca5c3ff15c
JavaDeveloper <Kozhevnikov95igor@gmail.com>
2022-07-25 18:04:14 +0300

    Третье задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {task1(); task2();}
+    public static void main(String[] args) {task1(); task2(); task3();}
 
     public static void task1() {
         // Задание 1
@@ -16,6 +16,7 @@ public class Main {
             System.out.print(b + " ");
             b--;
         }
+        System.out.println();
     }
 
     public static void task2() {
@@ -25,6 +26,19 @@ public class Main {
             System.out.println("Сегодня пятница, " + firstFriday + "-е число. Необходимо подготовить отчет.");
         }
 
+    }
+
+    public static void task3() {
+        //Задание 3
+
+        int firstYear = 0;
+        int beforeYear = 1822;
+        int futureYear = 2122;
+        for (; firstYear <= futureYear; firstYear = firstYear + 79) {
+            if (firstYear > beforeYear) {
+                System.out.println(firstYear);
+            }
+        }
     }
 
 
